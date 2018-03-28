99a1f69d8cd2087b47ebbadee3de6cbee7332499
Steven Yang <stevenwaterloo2015@gmail.com>
2018-03-28 13:44:28 -0400

    halfway through Todo API

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Todo, type: :model do
   # ensure Todo model has a 1:m relationship with the Item model
   it { should have_many(:items).dependent(:destroy) }
   # Validation tests
-  # ensure columns title and created_by are present before saving
+  # ensure columns title and create_by are present before saving
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:create_by) }
 end
